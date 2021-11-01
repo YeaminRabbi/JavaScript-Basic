46eee99ebb3b224608d5ac752ba738a265ecf84a
YeaminRabbi <yeaminrabbi308@gmail.com>
2021-11-01 11:32:07 +0600

    Primitive and Reference Values

--- 5. Object Literals/sandbox.js
+++ 5. Object Literals/sandbox.js
@@ -39,3 +39,46 @@ user.logout();
 //methods for the blogs
 user.logBlogs();
 
+
+
+
+////// Math Objects
+
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
+
+let area = 4.7;
+console.log(Math.round(area));
+console.log(Math.floor(area));
+console.log(Math.trunc(area));
+
+//random numbers
+const random = Math.random();
+
+//random number in a range of 1000
+console.log(Math.round(random * 1000));
+
+
+
+// Primitive vs Reference values
+
+let score1 = 50;
+let score2 =50;
+
+console.log(score1,score2);
+
+score1 =100;
+console.log(score1,score2);
+
+
+//reference type
+const userOne = { name:'Rabbi', age: 20};
+const userTwo = userOne;
+
+console.log(userOne, userTwo);
+
+userOne.age = 100;
+
+console.log(userOne, userTwo);
+
